901b8a82dcba922fe172060fdf99b10af108048c
fujiwara <shintaro.fujiwara@gmail.com>
2018-01-21 13:24:53 +0900

    Initialized segatexd-agent functionality 	modified:   ChangeLog 	modified:   Makefile.am 	new file:   agent/Makefile.am 	new file:   agent/main.c 	modified:   configure.ac 	deleted:    server/unused/daemonize_old.c 	deleted:    server/unused/getopt_long.h 	deleted:    server/unused/main_nss-pam-ldap-origin.c

---
+++ agent/main.c
@@ -0,0 +1,33 @@
+/*
+ *  main.c - main file for segatex-ng agent program.
+ *  This file contains the contents of segatex-ng.
+ *
+ *  Copyright (C) 2017-2018 Shintaro Fujiwara
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ *  02110-1301 USA
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ( int argc, char *argv [ ] )
+{
+    const char *app_name_agent = "segatexd-agent";
+
+    puts ( "This is segatexd-agent" );
+
+    return EXIT_SUCCESS;
+}
